67dfc3717fe331239ef761481ca409f3744aa5e4
Robert Važan <robert.vazan@protonmail.com>
2020-07-21 21:07:40 +0000

    Stub tests

--- src/main/java/com/machinezoo/pmdata/package-info.java
+++ src/main/java/com/machinezoo/pmdata/package-info.java
@@ -1,4 +1,4 @@
 // Part of PMData: https://pmdata.machinezoo.com
-@com.machinezoo.stagean.NoTests
+@com.machinezoo.stagean.StubTests
 @com.machinezoo.stagean.NoDocs
 package com.machinezoo.pmdata;
---
+++ src/test/java/com/machinezoo/pmdata/StubTest.java
@@ -0,0 +1,9 @@
+package com.machinezoo.pmdata;
+
+import org.junit.jupiter.api.*;
+
+public class StubTest {
+	@Test
+	public void stub() {
+	}
+}
