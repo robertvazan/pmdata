91ec2c24083fe6a98616b27344744f2149198480
Robert Važan <robert.vazan@protonmail.com>
2020-07-21 21:15:21 +0000

    Stub docs

--- src/main/java/com/machinezoo/pmdata/package-info.java
+++ src/main/java/com/machinezoo/pmdata/package-info.java
@@ -1,4 +1,7 @@
 // Part of PMData: https://pmdata.machinezoo.com
 @com.machinezoo.stagean.StubTests
-@com.machinezoo.stagean.NoDocs
+@com.machinezoo.stagean.StubDocs
+/**
+ * Framework for reactive data-driven and data science websites.
+ */
 package com.machinezoo.pmdata;
