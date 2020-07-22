476806fb10c3d5bd95f990570fe27772fc525352
Robert Važan <robert.vazan@protonmail.com>
2020-07-22 02:01:55 +0000

    Delayed initialization of MapDbPreferences

--- src/main/java/com/machinezoo/pmdata/MapDbPreferences.java
+++ src/main/java/com/machinezoo/pmdata/MapDbPreferences.java
@@ -10,25 +10,30 @@ import com.machinezoo.stagean.*;
 
 @DraftApi
 public class MapDbPreferences extends PreferenceStorage {
-	private static final DB db = DBMaker
-		.fileDB(SiteFiles.configOf(MapDbPreferences.class.getSimpleName()).resolve("preferences.db").toString())
-		.checksumHeaderBypass()
-		.transactionEnable()
-		.make();
-	private static final Map<String, String> map = ReactiveCollections.map(
-		db.hashMap("preferences", Serializer.STRING, Serializer.STRING).createOrOpen(),
-		new ReactiveCollections.Options()
-			.perItem()
-			.compareValues()
-			.ignoreWriteStatus()
-			.ignoreWriteExceptions());
+	/*
+	 * Force delayed initialization, so that we don't open the file just because some code mentions this class.
+	 */
+	private static class PreferencesFile {
+		private static final DB db = DBMaker
+			.fileDB(SiteFiles.configOf(MapDbPreferences.class.getSimpleName()).resolve("preferences.db").toString())
+			.checksumHeaderBypass()
+			.transactionEnable()
+			.make();
+		private static final Map<String, String> map = ReactiveCollections.map(
+			db.hashMap("preferences", Serializer.STRING, Serializer.STRING).createOrOpen(),
+			new ReactiveCollections.Options()
+				.perItem()
+				.compareValues()
+				.ignoreWriteStatus()
+				.ignoreWriteExceptions());
+	}
 	@Override
 	public String get(String key) {
-		return map.get(key);
+		return PreferencesFile.map.get(key);
 	}
 	@Override
 	public void set(String key, String value) {
-		map.put(key, value);
-		db.commit();
+		PreferencesFile.map.put(key, value);
+		PreferencesFile.db.commit();
 	}
 }
