58c12d479c9812d36a58d3a0172387b64864f4ab
Robert Važan <robert.vazan@tutanota.com>
2021-01-03 06:56:02 +0000

    Show class name under page title

--- src/main/java/com/machinezoo/pmdata/widgets/ArticleHeader.java
+++ src/main/java/com/machinezoo/pmdata/widgets/ArticleHeader.java
@@ -2,6 +2,7 @@
 package com.machinezoo.pmdata.widgets;
 
 import static java.util.stream.Collectors.*;
+import java.util.*;
 import com.machinezoo.pmsite.*;
 import com.machinezoo.pushmode.dom.*;
 import com.machinezoo.stagean.*;
@@ -9,14 +10,25 @@ import com.machinezoo.stagean.*;
 @DraftApi
 @DraftCode("structure & CSS cleanup")
 public class ArticleHeader {
+	private boolean showClass;
+	public ArticleHeader showClass(boolean showClass) {
+		this.showClass = showClass;
+		return this;
+	}
+	private final List<Class<?>> ignoredClasses = new ArrayList<>();
+	public ArticleHeader ignoreClass(Class<?> clazz) {
+		ignoredClasses.add(clazz);
+		return this;
+	}
 	protected String title() {
 		return SiteFragment.get().page().location().title();
 	}
 	private DomElement breadcrumbs() {
-		var location = SiteFragment.get().page().location();
+		var page = SiteFragment.get().page();
+		var location = page.location();
 		if (location.parent() == null)
 			return null;
-		return Html.div()
+		var crumbs = Html.div()
 			.clazz("article-header-breadcrumbs")
 			.add(DomFragment.join(" » ", location.ancestors().stream()
 				.filter(a -> !a.virtual() && a.path() != null && a.breadcrumb() != null)
@@ -26,6 +38,14 @@ public class ArticleHeader {
 				.collect(toList())))
 			.add(" » ")
 			.add(location.breadcrumb());
+		if (showClass && !ignoredClasses.contains(page.getClass())) {
+			crumbs
+				.add(" (")
+				.add(Html.code()
+					.add(page.getClass().getSimpleName()))
+				.add(")");
+		}
+		return crumbs;
 	}
 	public void render() {
 		SiteFragment.get()
