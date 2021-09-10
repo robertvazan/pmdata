63f43a77c7778a7827601d48e813fd84d411a242
Robert Važan <robert.vazan@tutanota.com>
2021-09-10 20:45:35 +0000

    No special map cache

--- src/main/java/com/machinezoo/pmdata/caching/KryoMapCache.java
+++
@@ -1,7 +0,0 @@
-// Part of PMData: https://pmdata.machinezoo.com
-package com.machinezoo.pmdata.caching;
-
-import java.util.*;
-
-public interface KryoMapCache<K, V> extends KryoCache<Map<K, V>>, PersistentMapSource<K, V> {
-}
