3b1ba05a5f7a9e27d59c4d48d5b976db13730b15
Robert Važan <robert.vazan@tutanota.com>
2023-04-08 12:57:06 +0000

    Updated project configuration: build PRs, updated CONTRIBUTING.md, upgraded stagean and pmsite, added UA banner

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,46 +1,41 @@
 # This script generates and updates project configuration files.
 
-# We are assuming that project-config is available in sibling directory.
-# Checkout from https://github.com/robertvazan/project-config
-import os.path
-import sys
-sys.path.append(os.path.normpath(os.path.join(__file__, '../../../project-config/src')))
+# Run this script with rvscaffold in PYTHONPATH
+import rvscaffold as scaffold
 
-from java import *
+class Project(scaffold.Java):
+    def script_path_text(self): return __file__
+    def repository_name(self): return 'pmdata'
+    def pretty_name(self): return 'PMData'
+    def pom_description(self): return "Framework for reactive data-driven and data science websites."
+    def inception_year(self): return 2020
+    def jdk_version(self): return 17
+    def has_javadoc(self): return False
+    def stagean_annotations(self): return True
+    
+    def dependencies(self):
+        yield from super().dependencies()
+        yield self.use_pmsite()
+        yield self.use_streamex()
+        # Used to serialize cache content output.
+        yield self.use('com.esotericsoftware:kryo:5.2.0')
+        yield self.use('org.jfree:jfreechart:1.5.3')
+        # Batik is used to render charts into SVG.
+        batik_version = '1.14'
+        yield self.use(f'org.apache.xmlgraphics:batik-dom:{batik_version}')
+        yield self.use(f'org.apache.xmlgraphics:batik-svggen:{batik_version}')
+        yield self.use(f'org.apache.xmlgraphics:batik-awt-util:{batik_version}')
+        yield self.use('com.github.haifengl:smile-plot:2.6.0')
+        yield self.use_junit()
+        yield self.use_slf4j_test()
+    
+    def javadoc_links(self):
+        yield 'https://stagean.machinezoo.com/javadoc/'
+        yield 'https://noexception.machinezoo.com/javadoc/'
+        yield 'https://hookless.machinezoo.com/javadocs/core/'
+        yield 'https://pushmode.machinezoo.com/javadoc/'
+        # PMSite does not have javadoc yet.
+        # JFreeChart and Kryo not linked, because automatic modules are not supported by javadoc.
+        # Smile plot not linked, because it is not a module yet.
 
-project_script_path = __file__
-repository_name = lambda: 'pmdata'
-pretty_name = lambda: 'PMData'
-pom_description = lambda: "Framework for reactive data-driven and data science websites."
-inception_year = lambda: 2020
-jdk_version = lambda: 17
-has_javadoc = lambda: False
-stagean_annotations = lambda: True
-project_status = lambda: experimental_status()
-
-def dependencies():
-    use_pmsite()
-    use_streamex()
-    # Used to serialize cache content output.
-    use('com.esotericsoftware:kryo:5.2.0')
-    use('org.jfree:jfreechart:1.5.3')
-    # Batik is used to render charts into SVG.
-    batik_version = '1.14'
-    use(f'org.apache.xmlgraphics:batik-dom:{batik_version}')
-    use(f'org.apache.xmlgraphics:batik-svggen:{batik_version}')
-    use(f'org.apache.xmlgraphics:batik-awt-util:{batik_version}')
-    use('com.github.haifengl:smile-plot:2.6.0')
-    use_junit()
-    use_slf4j_test()
-
-javadoc_links = lambda: [
-    'https://stagean.machinezoo.com/javadoc/',
-    'https://noexception.machinezoo.com/javadoc/',
-    'https://hookless.machinezoo.com/javadocs/core/',
-    'https://pushmode.machinezoo.com/javadoc/'
-    # PMSite does not have javadoc yet.
-    # JFreeChart and Kryo not linked, because automatic modules are not supported by javadoc.
-    # Smile plot not linked, because it is not a module yet.
-]
-
-generate(globals())
+Project().generate()
