37943711767f44d33cb1e014cb6bc094c15ce6c4
Robert Važan <robert.vazan@tutanota.com>
2023-09-20 18:50:39 +0000

    Removed files unrelated to caching

--- src/main/java/com/machinezoo/pmdata/package-info.java
+++
@@ -1,7 +0,0 @@
-// Part of PMData: https://pmdata.machinezoo.com
-/**
- * Framework for reactive data-driven and data science websites.
- */
-@com.machinezoo.stagean.StubTests
-@com.machinezoo.stagean.StubDocs
-package com.machinezoo.pmdata;
